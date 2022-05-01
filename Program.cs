e12d9b77d8b0df74051c42cf54989b3f74a2ed72
Jok55r <81425160+Jok55r@users.noreply.github.com>
2022-05-01 16:40:22 +0200

    enum update

--- Program.cs
+++ Program.cs
@@ -2,71 +2,56 @@
 
 namespace week42
 {
+    public enum Pixel
+    {
+        iShot = (char)250, //'·'
+        iHit = (char)88, //'X'
+        heShot = (char)250, //'·'
+        heHit = (char)177 //'░'
+    }
     internal class Program
     {
-        public const int width = 12;
-        public const int height = 23;
-        static char[,] field = new char[height, width];
-
+        public const int width = 12, height = 23;
         static bool xCordinatsNow = true;
-
-        static int howManyMyShips = 10;
-        static int howManyOppShips = 10;
-
-        static int xChoose = 0;
-        static int yChoose = 0;
-
-        static int needToWin = 10;
-        static int needToLose = 10;
-
-        static int[,] shipCorr = new int[height, width];
-
-        static char[] nums = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
         static bool yourTurn = true;
+
+        static sbyte howManyMyShips = 10, howManyOppShips = 10;
+        static sbyte needToWin = 10, needToLose = 10;
+
+        static sbyte xChoose = 0, yChoose = 0;
+
+        static char[,] field = new char[height, width];
+        static int[,] shipCorr = new int[height, width];
 
         static void Main(string[] args)
         {
             MakeField();
-            for (; ; )
+            while(true)
             {
                 Console.Clear();
                 DrawField();
                 ChooseCordinats();
                 BotHitYou();
                 CheckIfEnd();
-                xCordinatsNow = !xCordinatsNow;
             }
         }
-
         static void CheckIfEnd()
         {
-            if (needToWin == 0)
+            if (needToWin == 0 || needToLose == 0)
             {
                 Console.WriteLine();
-                Console.WriteLine("---You Win---");
-                Console.ReadLine();
-                Restart();
-            }
-            if (needToLose == 0)
-            {
-                Console.ReadLine();
-                Console.WriteLine("---You Lose---");
+                if (needToWin == 0)
+                    Console.WriteLine("---You Win---");
+                else
+                    Console.WriteLine("---You Lose---");
                 Console.ReadLine();
                 Restart();
             }
         }
-
         static void Restart()
         {
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = 0; j < width; j++)
-                {
-                    shipCorr[i, j] = 0;
-                }
-            }
-            xCordinatsNow = false;
+            Array.Clear(shipCorr, 0, shipCorr.Length);
+            xCordinatsNow = true;
             xChoose = 0;
             yChoose = 0;
             needToWin = 10;
@@ -74,7 +59,6 @@ namespace week42
             howManyMyShips = 10;
             MakeField();
         }
-
         static void BotHitYou()
         { 
             while (!yourTurn)
@@ -84,8 +68,8 @@ namespace week42
                 bool hitAnything = false;
                 for(; !hitAnything;)
                 {
-                    int xRand = rnd.Next(0, 10);
-                    int yRand = rnd.Next(12, 23);
+                    int xRand = rnd.Next(1, 11);
+                    int yRand = rnd.Next(12, 22);
                 
                     if (shipCorr[yRand, xRand] == 0)
                     {
@@ -103,45 +87,38 @@ namespace week42
                 if (!hitBoat) yourTurn = true;
             }
         }
-
         static void ChooseCordinats()
         {
             char input = Console.ReadKey().KeyChar;
 
-            if (input == '0' || input == '1' || input == '2' || input == '3' || input == '4' || input == '5' ||
-                input == '6' || input == '7' || input == '8' || input == '9' || input == 'r')
-            {
+            if (input == 'r') Restart();
 
-                if ((char)input == 'r')
-                {
-                    Restart();
-                }
-                else if (xCordinatsNow)
-                {
-                    xChoose = (int)input - 48;
-                }
-                else
-                {
-                    yChoose = (int)input - 48;
+            else if ((int)input >= '0' && (int)input <= '9')
+            {
+                if (xCordinatsNow)
+                    xChoose = (sbyte)(input - 48);
+
+                else {
+                    yChoose = (sbyte)(input - 48);
                     hitSomething();
                 }
+                xCordinatsNow = !xCordinatsNow;
             }
         }
-
         static void hitSomething()
         {
-            if (shipCorr[yChoose, xChoose] == 1)
+            yChoose++;
+            xChoose++;
+            if (shipCorr[yChoose, xChoose] == 1) 
             {
                 shipCorr[yChoose, xChoose] = 3;
                 needToWin--;
             }
-            else 
-            { 
-                shipCorr[yChoose + 1, xChoose + 1] = 2;
+            else { 
+                shipCorr[yChoose, xChoose] = 2;
                 yourTurn = false;
             }
         }
-
         static void MakeField()
         {
             Random rnd = new Random();
@@ -149,72 +126,49 @@ namespace week42
             {
                 for (int j = 0; j < width; j++)
                 {
-                    if (rnd.Next(0, 7) == 1 && howManyMyShips > 0 && i > height - 11 && i < height - 1 && j > 0 && j < width - 1)
-                    {
+                    if (rnd.Next(0, 7) == 1 && howManyMyShips > 0 && i > height - 11 && i < height - 1 && j > 0 && j < width - 1){
                         field[i, j] = '▓';
                         shipCorr[i, j] = 1;
                         howManyMyShips--;
                     }
-                    else if (rnd.Next(0, 7) == 1 && howManyOppShips > 0 && i < height - 12 && i > 0 && j > 0 && j < width - 1)
-                    {
+                    else if (rnd.Next(0, 7) == 1 && howManyOppShips > 0 && i < height - 12 && i > 0 && j > 0 && j < width - 1){
                         howManyOppShips--;
                         shipCorr[i, j] = 1;
                     }
-
                     else field[i, j] = ' ';
                 }
             }
         }
-
         static void DrawField()
         {
+            char[] nums = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
             for (int i = 0; i < height; i++)
             {
                 for (int j = 0; j < width; j++)
                 {
-                    //je korbl ale she ne vdaryv = 1
-                    //ja vdaryv = 2
-                    //ja popav = 3
-                    //vin vdaryv = 4
-                    //vin popav = 5
                     if (i == 0 || j == 0 || i == height - 1 || j == width - 1 || i == 11)
-                    {
                         field[i, j] = '█';
-                    }
-                    else if (shipCorr[i, j] == 2)
-                    {
-                        field[i, j] = ' ';
-                    }
-                    else if (shipCorr[i, j] == 3)
-                    {
-                        field[i, j] = 'X';
-                    }
-                    else if (shipCorr[i, j] == 4)
-                    {
-                        field[i, j] = '·';
-                    }
-                    else if (shipCorr[i, j] == 5)
-                    {
-                        field[i, j] = '░';
-                    }
-                    else if (i < 12)
-                    {
+                    else if ( i < 12)
                         field[i, j] = '▒';
-                    }
+
+                    PlacePixel(i, j);
 
                     if (xCordinatsNow && j > 0 && j < width - 1 && i == 0)
-                    {
                         field[i, j] = nums[j - 1];
-                    }
                     else if (!xCordinatsNow && i > 0 && i < 11 && j == 0)
-                    {
                         field[i, j] = nums[i - 1];
-                    }
-                    Console.Write(field[i, j]);
+
+                    Console.Write((char)field[i, j]);
                 }
-                if (i == 0) Console.Write($"    {xChoose}");
-                if (i == 1) Console.Write($"    {yChoose}");
                 Console.WriteLine();
+            }
+        }
+        static void PlacePixel(int i, int j)
+        {
+            for (int n = 0; n < 4; n++)
+            {
+                if (shipCorr[i, j] == n + 2)
+                    field[i, j] = (char)(Pixel)n;
             }
         }
     }
